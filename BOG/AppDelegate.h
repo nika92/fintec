b81d65431690ebb16b745e43e7e8d56e5cf09f38
Mariam Gagua <mariamkaygagua@gmail.com>
2018-09-15 11:02:16 +0400

    Initial Commit

---
+++ BOG/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BOG
+//
+//  Created by Mariam Gagua on 9/15/18.
+//  Copyright © 2018 Mariam Gagua. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BOG/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  BOG
+//
+//  Created by Mariam Gagua on 9/15/18.
+//  Copyright © 2018 Mariam Gagua. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
